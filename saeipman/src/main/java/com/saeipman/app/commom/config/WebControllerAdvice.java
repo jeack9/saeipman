9478bf2e883e4add7e01eae73b1fc09d910a9fd7
jeack9 <jeack99@g.yju.ac.kr>
2024-09-02 02:26:08 +0900

    임대인테스트중

---
+++ saeipman/src/main/java/com/saeipman/app/commom/config/WebControllerAdvice.java
@@ -0,0 +1,14 @@
+package com.saeipman.app.commom.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class WebControllerAdvice {
+	@ModelAttribute("contextPath")
+	public String contextPath(final HttpServletRequest request) {
+		return request.getContextPath();
+	}
+}
