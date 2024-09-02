fbf944b6e4d8903ab8e491b96197b1f82425fd7a
admin <admin@YD01-07>
2024-09-03 08:52:39 +0900

    테스트 오류 수정

--- saeipman/src/test/java/com/saeipman/app/SaeipmanApplicationTests.java
+++ saeipman/src/test/java/com/saeipman/app/SaeipmanApplicationTests.java
@@ -1,28 +1,10 @@
 package com.saeipman.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.saeipman.app.gwanlibi.mapper.MonthGwanlibiMapper;
-import com.saeipman.app.gwanlibi.service.MonthGwanlibiVO;
 
 @SpringBootTest
 class SaeipmanApplicationTests {
 	
-	
-	@Autowired
-	MonthGwanlibiMapper monthGwanlibiMapper;
-	
-	@Test
-	void buildingList() {
-		List<MonthGwanlibiVO> list = monthGwanlibiMapper.selectAllMonGwanlibiList("10");
-		assertTrue(!list.isEmpty());
-		System.err.println(list.size());
-	}
+
 
 }
