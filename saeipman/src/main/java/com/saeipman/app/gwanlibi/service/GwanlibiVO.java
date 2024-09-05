3bcacddb94526f93dce5c0d3d206d046c567e72b
YD01-08\admin <yoonn1217@gmailcom>
2024-09-05 18:13:28 +0900

    관리비 상세 내역, date picker (toast ui)

--- saeipman/src/main/java/com/saeipman/app/gwanlibi/service/GwanlibiVO.java
+++ saeipman/src/main/java/com/saeipman/app/gwanlibi/service/GwanlibiVO.java
@@ -1,5 +1,9 @@
 package com.saeipman.app.gwanlibi.service;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -8,7 +12,8 @@ public class GwanlibiVO {
 	private String monthGwanlibiNo;       // 월 관리비 번호 - PK
 	private String buildingId;			  // 건물 식별 ID - FK
 	private int totalMoney; 			  // 관리비 총 금액
-	private String paymentMonth;		  // 납부 월
+	@DateTimeFormat(pattern = "yyyy-MM")
+	private Date paymentMonth;		  // 납부 월
 	private int gaguGwanlibi; 			  // 가구별 관리비
 	private String buildingName;
 	private int previousMonth;
