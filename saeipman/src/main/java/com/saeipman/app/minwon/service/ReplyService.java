3f2e87bfcd26ac158eedb67330ce15e23077461b
juhee777 <l55117845@gmail.com>
2024-09-08 23:37:56 +0900

    Update ReplyService.java

--- saeipman/src/main/java/com/saeipman/app/minwon/service/ReplyService.java
+++ saeipman/src/main/java/com/saeipman/app/minwon/service/ReplyService.java
@@ -3,9 +3,16 @@ package com.saeipman.app.minwon.service;
 import java.util.List;
 
 public interface ReplyService {
-	public int cmtInsert(ReplyVO vo);
-	public ReplyVO cmtList(Long minwonCmtNo);
-	public int cmtDelete(Long minwonCmtNo);
-	public List<ReplyVO> withPagingList(Long postNo);
+    // 새로운 댓글 등록
+    void insertCmt(ReplyVO vo);
+
+    // 대댓글 등록
+    void insertReplyCmt(ReplyVO vo);
+
+    // 댓글 및 대댓글 리스트 조회
+    List<ReplyVO> listCmt(Long postNo);
+
+    // 댓글 삭제
+    void deleteCmt(Long minwonCmtNo);
 	
 }
