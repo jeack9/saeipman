65c9de2e4f82b2fdab4739f44a30361324083240
YD01-08\admin <yoonn1217@gmailcom>
2024-09-09 18:21:58 +0900

    24-09-09 / 관리비 페이지 수정

--- saeipman/src/main/java/com/saeipman/app/commom/security/config/SecurityConfig2.java
+++ saeipman/src/main/java/com/saeipman/app/commom/security/config/SecurityConfig2.java
@@ -1,112 +1,112 @@
-package com.saeipman.app.commom.security.config;
-
-import java.util.Collections;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import com.saeipman.app.commom.jwt.JwtFilter;
-import com.saeipman.app.commom.jwt.JwtUtil;
-import com.saeipman.app.commom.jwt.LoginFilter;
-
-import jakarta.servlet.DispatcherType;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig2 {
-	// AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
-	private final AuthenticationConfiguration authenticationConfiguration;
-	private final JwtUtil jwtUtill;
-
-	@Bean // 비밀번호 암호화
-	BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	// AuthenticationManager Bean 등록
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
-	RoleHierarchy roleHierarchy() {
-		RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
-		hierarchy.setHierarchy("""
-				ROLE_0 > ROLE_1
-				ROLE_1 > ROLE_2
-				""");
-		return hierarchy;
-	}
-
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// cors 
-		http
-			.cors((cors) -> cors
-					.configurationSource(new CorsConfigurationSource() {
-						@Override
-						public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-		                    CorsConfiguration configuration = new CorsConfiguration();
-
-		                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-		                    configuration.setAllowedMethods(Collections.singletonList("*"));
-		                    configuration.setAllowCredentials(true);
-		                    configuration.setAllowedHeaders(Collections.singletonList("*"));
-		                    configuration.setMaxAge(3600L);
-
-							configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-		                    return configuration;	
-						}
-					}));
-		
-		// csrf disable
-		http
-			.csrf(auth -> auth.disable());
-		
-		// From 로그인 방식 disable
-		http.formLogin((auth) -> auth.disable());
-
-		// http basic 인증 방식 disable
-		http.httpBasic((auth) -> auth.disable());
-
-		// 경로별 인가 작업
-		http
-			.authorizeHttpRequests((auth) -> auth.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-			// 정적 리소스 허용
-			.requestMatchers("/css/**", "/img/**", "/js/**", "/lib/**", "/scss/**", "/dashmin-1.0.0/**").permitAll()
-			.requestMatchers("/member/login", "/", "/member/join", "/join", "/login").permitAll()
-			.requestMatchers("/member/**").hasAnyAuthority("ROLE_1")
-			.requestMatchers("/admin/**").hasRole("0")
-			.anyRequest().authenticated());
-
-		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
-		http
-	    	.addFilterBefore(new JwtFilter(jwtUtill), LoginFilter.class);
-		http
-		    .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtill), UsernamePasswordAuthenticationFilter.class);
-
-		// 세션 설정
-		http
-			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-		return http.build();
-	}
-}
+//package com.saeipman.app.commom.security.config;
+//
+//import java.util.Collections;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//
+//import com.saeipman.app.commom.jwt.JwtFilter;
+//import com.saeipman.app.commom.jwt.JwtUtil;
+//import com.saeipman.app.commom.jwt.LoginFilter;
+//
+//import jakarta.servlet.DispatcherType;
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.RequiredArgsConstructor;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig2 {
+//	// AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+//	private final AuthenticationConfiguration authenticationConfiguration;
+//	private final JwtUtil jwtUtill;
+//
+//	@Bean // 비밀번호 암호화
+//	BCryptPasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	// AuthenticationManager Bean 등록
+//	@Bean
+//	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//		return configuration.getAuthenticationManager();
+//	}
+//
+//	@Bean
+//	RoleHierarchy roleHierarchy() {
+//		RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+//		hierarchy.setHierarchy("""
+//				ROLE_0 > ROLE_1
+//				ROLE_1 > ROLE_2
+//				""");
+//		return hierarchy;
+//	}
+//
+//	@Bean
+//	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		// cors 
+//		http
+//			.cors((cors) -> cors
+//					.configurationSource(new CorsConfigurationSource() {
+//						@Override
+//						public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//
+//		                    CorsConfiguration configuration = new CorsConfiguration();
+//
+//		                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//		                    configuration.setAllowedMethods(Collections.singletonList("*"));
+//		                    configuration.setAllowCredentials(true);
+//		                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+//		                    configuration.setMaxAge(3600L);
+//
+//							configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//
+//		                    return configuration;	
+//						}
+//					}));
+//		
+//		// csrf disable
+//		http
+//			.csrf(auth -> auth.disable());
+//		
+//		// From 로그인 방식 disable
+//		http.formLogin((auth) -> auth.disable());
+//
+//		// http basic 인증 방식 disable
+//		http.httpBasic((auth) -> auth.disable());
+//
+//		// 경로별 인가 작업
+//		http
+//			.authorizeHttpRequests((auth) -> auth.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+//			// 정적 리소스 허용
+//			.requestMatchers("/css/**", "/img/**", "/js/**", "/lib/**", "/scss/**", "/dashmin-1.0.0/**").permitAll()
+//			.requestMatchers("/member/login", "/", "/member/join", "/join", "/login").permitAll()
+//			.requestMatchers("/member/**").hasAnyAuthority("ROLE_1")
+//			.requestMatchers("/admin/**").hasRole("0")
+//			.anyRequest().authenticated());
+//
+//		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+//		http
+//	    	.addFilterBefore(new JwtFilter(jwtUtill), LoginFilter.class);
+//		http
+//		    .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtill), UsernamePasswordAuthenticationFilter.class);
+//
+//		// 세션 설정
+//		http
+//			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//		return http.build();
+//	}
+//}
--- saeipman/src/main/java/com/saeipman/app/gwanlibi/service/GwanlibiVO.java
+++ saeipman/src/main/java/com/saeipman/app/gwanlibi/service/GwanlibiVO.java
@@ -13,17 +13,16 @@ public class GwanlibiVO {
 	private String buildingId;			  // 건물 식별 ID - FK
 	private int totalMoney; 			  // 관리비 총 금액
 	@DateTimeFormat(pattern = "yyyy-MM")
-	private Date paymentMonth;		  // 납부 월
+	private Date paymentMonth;  		  // 관리비 납부 월	
+	private String buildingName; 		  // 건물 이름
+	private int previousMonth;			  // 전월 관리 비용
+	private int thisMonth;				  // 금월 관리 비용
+	private String gwanlibiName;		  // 관리비 이름
+	
 	private int gaguGwanlibi; 			  // 가구별 관리비
-	private String buildingName;
-	private int previousMonth;
-	private int thisMonth;
-	private double gwanlibiByGagu;
-	private String strGwanlibiByGagu;
+	private double gwanlibiByGagu;        // 가구별 관리비
+	private String strGwanlibiByGagu;	  // 가구별 관리비 (,)
 	
-	
-	private String gwanlibiName;
-	
-	private int gwanlibiItemMoney;
-	private String strGwanlibiItemMoney;
+	private int gwanlibiItemMoney;		  // 항목별 관리비
+	private String strGwanlibiItemMoney;  // 항목별 관리비 (,)
 }
