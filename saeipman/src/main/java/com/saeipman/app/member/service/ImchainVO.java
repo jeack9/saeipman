f8303ced300508bb7d5eea6c1f82ecc0102f9236
admin <admin@YD01-12>
2024-09-10 18:42:01 +0900

    방관리 디자인중

---
+++ saeipman/src/main/java/com/saeipman/app/member/service/ImchainVO.java
@@ -0,0 +1,12 @@
+package com.saeipman.app.member.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImchainVO {
+	private String imchainId;
+	private String imchainName;
+	private int roomNo;
+}
---
+++ saeipman/src/main/java/com/saeipman/app/room/service/impl/RoomServiceImpl.java
@@ -0,0 +1,7 @@
+package com.saeipman.app.room.service.impl;
+
+import com.saeipman.app.room.service.RoomService;
+
+public class RoomServiceImpl implements RoomService {
+
+}
