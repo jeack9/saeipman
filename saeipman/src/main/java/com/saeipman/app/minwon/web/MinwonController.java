df8e509b5032c4b6d8b2f793e264feb9eba5a5ba
= <=>
2024-09-11 19:20:25 +0900

    문자 테스트

--- saeipman/src/main/java/com/saeipman/app/minwon/web/MinwonController.java
+++ saeipman/src/main/java/com/saeipman/app/minwon/web/MinwonController.java
@@ -1,7 +1,5 @@
 package com.saeipman.app.minwon.web;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -12,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,27 +26,45 @@ import com.saeipman.app.minwon.service.MinwonService;
 import com.saeipman.app.minwon.service.MinwonVO;
 import com.saeipman.app.minwon.service.PageDTO;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MinwonController {
-	
-	private MinwonService minwonService;
-	
-	
+
+	private final MinwonService minwonService;
+
+
 	@Value("${file.upload.path}")
 	private String uploadPath;
 	
-	@Autowired
-	public MinwonController(MinwonService minwonService) {
-		this.minwonService = minwonService;
-	}
-	
+	/*@Value("${coolsms.api.key}")
+	private String apiKey;
 
+	@Value("${coolsms.api.secret}")
+	private String apiSecret;
+
+	@Value("${coolsms.api.number}")
+	private String fromPhoneNumber;
+*/
+	private DefaultMessageService messageService;
+/*
+	@PostConstruct
+	private void init() {
+		this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
+	}
+*/
 	// 전체조회
 	@GetMapping("minwonList")
-	public String minwonList(Criteria cri ,Model model) {
+	public String minwonList(Criteria cri, Model model) {
 		cri.setAmount(5);
 		List<MinwonVO> list = minwonService.minwonList(cri);
 		model.addAttribute("minwon", list);
@@ -57,30 +72,30 @@ public class MinwonController {
 		model.addAttribute("pageMaker", new PageDTO(cri, total));
 		return "minwon/minwonList";
 	}
-	
+
 	// 단건조회
 	@GetMapping("minwonInfo")
 	public String minwonInfo(@ModelAttribute Criteria cri, MinwonVO minwonVO, Model model) {
 		MinwonVO findVO = minwonService.minwonSelect(minwonVO);
-		model.addAttribute("minwon",findVO);
+		model.addAttribute("minwon", findVO);
 		model.addAttribute("cri", cri);
 		return "minwon/minwonInfo";
 	}
-	
-	//민원 상태 업로드
+
+	// 민원 상태 업로드
 	@PostMapping("/updateMinwonState")
-	public String updateMinwonState(@ModelAttribute Criteria cri, MinwonVO minwonVO, @RequestParam("state") String state) {
-	     // VO 객체에 상태값 세팅
-	     minwonVO.setAcceptState(state);
+	public String updateMinwonState(@ModelAttribute Criteria cri, MinwonVO minwonVO,
+			@RequestParam("state") String state) {
+		// VO 객체에 상태값 세팅
+		minwonVO.setAcceptState(state);
 
-	     // 서비스 계층을 통해 상태 업데이트
-	     minwonService.acceptStateUpdate(minwonVO);
+		// 서비스 계층을 통해 상태 업데이트
+		minwonService.acceptStateUpdate(minwonVO);
 
-	     // 처리 후 민원 상세 페이지로 리다이렉트
-	     return "redirect:/minwonInfo?postNo=" + minwonVO.getPostNo() + "&pageNum=" + cri.getPageNum();
+		// 처리 후 민원 상세 페이지로 리다이렉트
+		return "redirect:/minwonInfo?postNo=" + minwonVO.getPostNo() + "&pageNum=" + cri.getPageNum();
 	}
-	
-	
+
 	// 등록(페이지)
 	@GetMapping("minwonInsert")
 	public String minwonInsertForm(Model model) {
@@ -88,98 +103,121 @@ public class MinwonController {
 		model.addAttribute("categories", category);
 		return "minwon/minwonInsert";
 	}
+
 	// 등록(처리)
 	@PostMapping("minwonInsert")
-	public String minwonInsert(@RequestPart MultipartFile[] files ,MinwonVO minwonVO) {
+	public String minwonInsert(@RequestPart MultipartFile[] files, MinwonVO minwonVO) {
 		minwonVO.setRoomId("2");
 		minwonVO.setRoomNo("101");
-		
-		//dgsa
+
 		List<String> imgList = new ArrayList<>();
-		
+
 		log.info(uploadPath);
-		for(MultipartFile file : files) {
+		for (MultipartFile file : files) {
 			log.info(file.getContentType());
 			log.info(file.getOriginalFilename());
 			log.info(String.valueOf(file.getSize()));
-			
-			String fileName = file.getOriginalFilename(); 
-			String saveName = uploadPath + File.separator + fileName; //separator = 자바가 인식하는 경로
-			
+
+			String fileName = file.getOriginalFilename();
+			String saveName = uploadPath + File.separator + fileName; // separator = 자바가 인식하는 경로
+
 			log.debug("saveName : " + saveName);
-			
-			Path savePath = Paths.get(saveName); 
-			
+
+			Path savePath = Paths.get(saveName);
+
 			try {
 				file.transferTo(savePath);
 				imgList.add(fileName);
-			}catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 		minwonVO.setGroupId(String.join(":", imgList));
 		minwonService.minwonInsert(minwonVO);
 		return "redirect:minwonList";
+
 	}
+
+
 	// 수정(페이지)
 	@GetMapping("minwonUpdate")
 	public String boardUpdateForm(MinwonVO minwonVO, Model model) {
 		MinwonVO findVO = minwonService.minwonSelect(minwonVO);
-		model.addAttribute("minwon",findVO);
+		model.addAttribute("minwon", findVO);
 		List<MinwonVO> ca = minwonService.categoryList();
-		model.addAttribute("categories",ca);
+		model.addAttribute("categories", ca);
 		return "minwon/minwonUpdate";
 
-		
 	}
+	/*
+	// 보내는 메시지 내용
+	@PostMapping("/send-one")
+	@ResponseBody
+	public String sendOne() {
+		Message message = new Message();
+
+		message.setFrom(fromPhoneNumber); // 발신 번호
+		message.setTo("01092086643");
+		message.setText("인증코드는 안준모 입니다");
+
+		SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+		System.out.println(response);
+		if (response.getStatusCode().equals("2000")) { // 2000은 정상으로 발송된 statusCode 번호임
+			return "성공";
+
+		} else {
+			return "bad"; // 발송 오류 모달창
+		}
+	}
+*/
 	// 수정(처리)
 	@PostMapping("minwonUpdate")
 	@ResponseBody
-	public List<String> updatList(@RequestPart MultipartFile[] files, MinwonVO minwonVO){
+	public List<String> updatList(@RequestPart MultipartFile[] files, MinwonVO minwonVO) {
 		List<String> imageList = new ArrayList<>();
-		
-	    for(MultipartFile uploadFile : files){
-	    	if(uploadFile.getContentType().startsWith("image") == false){
-	    		System.err.println("this file is not image type");
-	    		return null;
-	        }
-	  
-	        String fileName = uploadFile.getOriginalFilename();
-	        System.out.println("fileName : " + fileName);
-	    
-	        //날짜 폴더 생성
-	        String folderPath = makeFolder(); //
-	        //UUID
-	        String uuid = UUID.randomUUID().toString();
-	        //저장할 파일 이름 중간에 "_"를 이용하여 구분
-	        
-	        String uploadFileName = folderPath +File.separator + uuid + "_" + fileName;
-	        
-	        String saveName = uploadPath + File.separator + uploadFileName;
-	        
-	        Path savePath = Paths.get(saveName);
-	        //Paths.get() 메서드는 특정 경로의 파일 정보를 가져옵니다.(경로 정의하기)
-	        System.out.println("path : " + saveName);
-	        try{
-	        	uploadFile.transferTo(savePath);
-	            //uploadFile에 파일을 업로드 하는 메서드 transferTo(file)
-	        } catch (IOException e) {
-	             e.printStackTrace();	             
-	        }
-	        // DB에 해당 경로 저장
-	        // 1) 사용자가 업로드할 때 사용한 파일명
-	        // 2) 실제 서버에 업로드할 때 사용한 경로
-	        // 데이터베이스에 이미지 경로 저장 (이미지 경로 수정)
-	        minwonVO.setGroupId(String.join(":", imageList));
-	        
-	        // 서비스에서 업데이트 처리
-	        minwonService.minwonUpdate(minwonVO);
-	     }
-	    
-	    return imageList;
+
+		for (MultipartFile uploadFile : files) {
+			if (uploadFile.getContentType().startsWith("image") == false) {
+				System.err.println("this file is not image type");
+				return null;
+			}
+
+			String fileName = uploadFile.getOriginalFilename();
+			System.out.println("fileName : " + fileName);
+
+			// 날짜 폴더 생성
+			String folderPath = makeFolder(); //
+			// UUID
+			String uuid = UUID.randomUUID().toString();
+			// 저장할 파일 이름 중간에 "_"를 이용하여 구분
+
+			String uploadFileName = folderPath + File.separator + uuid + "_" + fileName;
+
+			String saveName = uploadPath + File.separator + uploadFileName;
+
+			Path savePath = Paths.get(saveName);
+			// Paths.get() 메서드는 특정 경로의 파일 정보를 가져옵니다.(경로 정의하기)
+			System.out.println("path : " + saveName);
+			try {
+				uploadFile.transferTo(savePath);
+				// uploadFile에 파일을 업로드 하는 메서드 transferTo(file)
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// DB에 해당 경로 저장
+			// 1) 사용자가 업로드할 때 사용한 파일명
+			// 2) 실제 서버에 업로드할 때 사용한 경로
+			// 데이터베이스에 이미지 경로 저장 (이미지 경로 수정)
+			minwonVO.setGroupId(String.join(":", imageList));
+
+			// 서비스에서 업데이트 처리
+			minwonService.minwonUpdate(minwonVO);
+		}
+
+		return imageList;
 	}
-	
+
 	private String makeFolder() {
 		String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 		// LocalDate를 문자열로 포멧
@@ -199,12 +237,11 @@ public class MinwonController {
 		return uploadFileName.replace(File.separator, "/");
 	}
 
-
 	// 삭제(처리)
 	@GetMapping("minwonDelete")
 	public String minwonDelete(Integer postNo) {
 		minwonService.minwonDelete(postNo);
 		return "redirect:minwonList";
 	}
-		
+
 }
