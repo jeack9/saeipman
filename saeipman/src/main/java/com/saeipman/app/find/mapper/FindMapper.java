523703afa21cd93fce1434ca532bec5dcce3e8d8
= <=>
2024-09-20 19:19:57 +0900

    인증번호, 문자

--- saeipman/src/main/java/com/saeipman/app/find/mapper/FindMapper.java
+++ saeipman/src/main/java/com/saeipman/app/find/mapper/FindMapper.java
@@ -6,5 +6,6 @@ import com.saeipman.app.find.service.FindVO;
 
 public interface FindMapper {
 	public FindVO selectId(FindVO findVO);
+	public FindVO seletPw(FindVO findVO);
 	public String updatePw(FindVO fivdVo);
 }
--- saeipman/src/main/java/com/saeipman/app/find/service/FindService.java
+++ saeipman/src/main/java/com/saeipman/app/find/service/FindService.java
@@ -3,4 +3,5 @@ package com.saeipman.app.find.service;
 
 public interface FindService {
 	public FindVO idSelect(FindVO findVO); //아이디 찾기
+	public FindVO pwSelect(FindVO findVO);
 }
