2808b2d37cbb8295b94a6e3097b38f0e818af841
admin <admin@YD01-12>
2024-10-01 12:23:41 +0900

    q

--- saeipman/src/main/java/com/saeipman/app/test/web/TestController.java
+++ saeipman/src/main/java/com/saeipman/app/test/web/TestController.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.saeipman.app.commom.security.SecurityUtil;
+
 
 
 @Controller
 public class TestController {
 	@GetMapping("/")
 	public String test() {
-		return "redirect:/member/home";
+		int auth = SecurityUtil.getLoginAuth();
+		String url = auth == 1 ? "redirect:/member/home" : "redirect:/paymentInfo";
+		return url;
 	}
 	
 	@GetMapping("/api/test")
